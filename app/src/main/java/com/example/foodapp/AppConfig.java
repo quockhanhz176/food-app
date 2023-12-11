30e53778458b53d607449cb7f266be1862e553bd
quockhanh.z176 <quockhanh.z176@gmail.com>
2023-12-11 14:12:09 +0700

    add api key

--- app/src/main/java/com/example/foodapp/AppConfig.java
+++ app/src/main/java/com/example/foodapp/AppConfig.java
@@ -4,5 +4,5 @@ import android.provider.MediaStore;
 
 public class AppConfig {
     // Environment variables
-    public static final String RECIPE_API_KEY = "9d2d0547037c47fb80e2b4b67f6dd801";
+    public static final String RECIPE_API_KEY = BuildConfig.RECIPE_KEY;
 }
